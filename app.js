b403afcbd8690e85976fc3e9ea70ba9e68324ed7
djromasanta <rdaltonjay@gmail.com>
2020-12-06 22:21:04 +0800

    Updates

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(function(req, res, next) {
 
 
 /* ROUTES */
-app.use('/main/rescuetime', rescuetime);
+//app.use('/main/rescuetime', rescuetime);
 app.use('/main/fitbit', fitbit);
-app.use('/main/oura', oura);
+//app.use('/main/oura', oura);
 
 app.get('/', function(req, res) {
   res.send({
